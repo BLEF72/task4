3f7235eec3122b33c84a4e41d194367939720bf4
BLEF <121230919+BLEF72@users.noreply.github.com>
2026-06-18 12:18:11 +0100

    fix session cookie

--- server/index.js
+++ server/index.js
@@ -69,11 +69,11 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-  },
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: 'none',
+},
 }));
 
 async function requireAuth(req, res, next) {
